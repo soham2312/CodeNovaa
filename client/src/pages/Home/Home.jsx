c8372d8561630a45be00ce2156f0b9df627b9dcf
utkarsh_raj <rajutkarsh2505@gmail.com>
2023-02-25 16:50:43 +0530

    made header and question component

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Question from "../../components/Question/Question";
 import { Link } from "react-router-dom";
-import Header from "../../components/Header/Header";
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <Question />
       <Link to="me">profile</Link>
     </div>
   );
